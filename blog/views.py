d55a6cfa91b036e93e02481c51e54b96fd0ce858
J Vishnu Vardhan Sai <vishnuabd278@gmail.com>
2020-07-13 23:22:01 +0530

    added admin panel and some templates,models, created some tables

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+def home(request):
+    return render(request,'blog/home.html')
+
+def products(request):
+    return render(request,'blog/products.html')
+
+def customers(request):
+    return render(request,'blog/customer.html')
+    
